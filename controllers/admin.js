5dd379982fda4bc738577bf56c1c5e982d48e767
João Victor Matias de Oliveira <joaovictor.a4@gmail.com>
2022-11-11 13:44:12 -0300

    updates

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,20 @@
 const Prodcut = require('../Models/product');
 exports.getProducts = (req, res, next) => {
-    Prodcut.fetchAll(product =>{
-        res.render('admin/products', {pageTitle: 'products', title: product,  path: '/admin/products'})
+    Prodcut.fetchAll(product => {
+        res.render('admin/products', { pageTitle: 'products', title: product, path: '/admin/products' })
     });
-  
+
+}
+exports.getEditProducts = (req, res, next) => {
+    const edit = req.query.edit
+    if (!edit) {
+        return res.redirect('/')
+    }
+    const prodID = req.params.productId
+    Prodcut.findId(prodID, product => {
+        if (!product) {
+            return res.redirect('/')
+        }
+        res.render('admin/edit-products', { pageTitle: 'edit-products', title: 'Edit', path: 'admin/edit-products', editing: edit, prod: product })
+    })
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const productController = require('../controllers/products')
 const adminController = require('../controllers/admin.js')
 
+
 router.get('/add', productController.getAddProduct);
 router.post('/add', productController.postProduct );
 router.get('/products', adminController.getProducts);
+router.get('/edit-products:productId', adminController.getEditProducts)
 exports.routes = router;
